84d4bfaad886fc1b24343cc1db49c2e33a22b076
Aban Khan <khanaban505@gmail.com>
2023-12-20 20:55:07 -0500

    Tippy still won't work

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
-import tippy from 'tippy.js';
-import 'tippy.js/dist/tippy.css';
+import tippy from '/tippy.js';
+/* import 'tippy.js/dist/tippy.css';
+import 'tippy.js/animations/scale.css'; */
 
 // Tippy for popups
 
